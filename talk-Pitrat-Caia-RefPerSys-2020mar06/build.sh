3f2bbf385456e10d879e49cf19bfd6977144b663
Basile Starynkevitch <basile@starynkevitch.net>
2020-03-04 12:58:52 +0100

    slides building in talk-Pitrat-Caia-RefPerSys-2020mar06

--- talk-Pitrat-Caia-RefPerSys-2020mar06/build.sh
+++ talk-Pitrat-Caia-RefPerSys-2020mar06/build.sh
@@ -6,12 +6,12 @@ for svgfile in *.svg ; do
     inkscape --without-gui --export-pdf=$svgbase.pdf $svgfile
     inkscape --without-gui --export-eps=$svgbase.eps $svgfile
 done
-for dotfile in *.dot ; do
-    dotbase=$(basename $dotfile .dot)
-    dot -v -Teps -o $dotbase.eps  $dotfile
-    dot -v -Tpdf -o $dotbase.pdf  $dotfile
-    dot -v -Tsvg -o $dotbase.svg  $dotfile
-done
+# for dotfile in *.dot ; do
+#     dotbase=$(basename $dotfile .dot)
+#     dot -v -Teps -o $dotbase.eps  $dotfile
+#     dot -v -Tpdf -o $dotbase.pdf  $dotfile
+#     dot -v -Tsvg -o $dotbase.svg  $dotfile
+# done
 lualatex --shell-escape  --halt-on-error Starynkevitch-CAIA-RefPerSys-2020mar06
 lualatex --shell-escape  --halt-on-error Starynkevitch-CAIA-RefPerSys-2020mar06.tex
 
